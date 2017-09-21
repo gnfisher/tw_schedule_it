e62e0d094092bf9d1e1c77c0354888f0f37ff1e9
Greg Fisher <me@gnfisher.com>
2017-09-21 17:29:23 -0300

    Add exe

--- lib/tw_schedule_it.rb
+++ lib/tw_schedule_it.rb
@@ -1,5 +1,17 @@
-require "tw_schedule_it/version"
+require "tw_schedule_it/tw_schedule_it/version"
 
 module TwScheduleIt
-  # Your code goes here...
+  class CLI
+    # $ schedule_it talks.txt
+    # Event name:
+    # Event description:
+    # ...
+    # Menu:
+    # 1. View full schedule
+    # 2. View full list of talks
+    #
+    def initialize(argv)
+      puts argv
+    end
+  end
 end
