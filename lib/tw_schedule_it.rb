101f1e4a88cdd97dc630d56ce7a049faf681ff5c
Greg Fisher <me@gnfisher.com>
2017-09-23 12:59:39 -0300

    Remove App class totally

--- lib/tw_schedule_it.rb
+++ lib/tw_schedule_it.rb
@@ -4,24 +4,3 @@ require "tw_schedule_it/import_from_file"
 require "tw_schedule_it/schedule"
 require "tw_schedule_it/talks"
 require "tw_schedule_it/talks_factory"
-
-# module TwScheduleIt
-#   class App
-#     attr_reader :talks_file_path, :event
-#
-#     def initialize(talks_file_path)
-#       @talks_file_path = talks_file_path
-#     end
-#
-#     private
-#
-#     def talks_data
-#       TwScheduleIt::ImportFromFile.build(talks_file_path)
-#     end
-#
-#     def event
-#       @event ||= TwScheduleIt::Event.new(
-#         talks: TwScheduleIt::TalksFactory.build(talks_data))
-#     end
-#   end
-# end
