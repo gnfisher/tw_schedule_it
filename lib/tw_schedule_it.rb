8306a6d11a952c72e78a4d0ece4a6061707a0f6d
Greg Fisher <me@gnfisher.com>
2017-09-23 13:03:48 -0300

    Enforce Ruby version > 2.0

--- lib/tw_schedule_it.rb
+++ lib/tw_schedule_it.rb
@@ -4,3 +4,5 @@ require "tw_schedule_it/import_from_file"
 require "tw_schedule_it/schedule"
 require "tw_schedule_it/talks"
 require "tw_schedule_it/talks_factory"
+
+raise 'Ruby version must be greater than 2.0' unless RUBY_VERSION.to_f > 2.0
