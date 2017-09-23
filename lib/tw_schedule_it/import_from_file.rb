3b89b8c3618502a37116fd5643af76de2c33e99a
Greg Fisher <me@gnfisher.com>
2017-09-23 12:02:12 -0300

    Handle blank lines

--- lib/tw_schedule_it/import_from_file.rb
+++ lib/tw_schedule_it/import_from_file.rb
@@ -2,12 +2,14 @@ module TwScheduleIt
   module ImportFromFile
     def self.build(file_path)
       file_lines = File.new(file_path).readlines
-      file_lines.map { |line| parse(line) }
+      file_lines.map { |line| parse(line) }.compact
     end
 
     private
 
     def self.parse(string)
+      return if string.strip.empty?
+
       if /\d/ =~ string # Is there any digit in string?
         title    = string.strip
         duration = string[/\d+/].strip.to_i
--- spec/import_from_file_spec.rb
+++ spec/import_from_file_spec.rb
@@ -1,28 +1,36 @@
 require "spec_helper"
 require "tw_schedule_it/import_from_file"
 
+Pry.config.history.should_save = false
+Pry.config.history.should_load = false
+
 RSpec.describe TwScheduleIt::ImportFromFile, fakefs: true do
-  def stub_talks_data
+  def stub_talks_data(data)
     FileUtils.mkdir("/tmp")
     File.open("/tmp/talk_data.txt", "w") do |f|
-      f.puts "Ruby on Rails Legacy App Maintenance 60min"
-      f.puts "A World Without HackerNews 30min"
-      f.puts "User Interface CSS in Rails Apps 30min"
-      f.puts "Rails for Python Developers lightning"
+      data.each { |line| f.puts "#{line[0]}" }
     end
   end
 
   let(:expected_data) do
-    [['Ruby on Rails Legacy App Maintenance', 60],
-     ['A World Without HackerNews',           30],
-     ['User Interface CSS in Rails Apps',     30],
-     ['Rails for Python Developers',          5]]
+    [['Ruby on Rails Legacy App Maintenance 60min', 60],
+     ['A World Without HackerNews 30min',           30],
+     ['User Interface CSS in Rails Apps 30min',     30],
+     ['Rails for Python Developers lightning',       5]]
   end
 
   it 'returns a nested array with the talk data' do
-    stub_talks_data
+    stub_talks_data(expected_data)
 
     talk_data = TwScheduleIt::ImportFromFile.build('/tmp/talk_data.txt')
     expect(talk_data).to match_array(expected_data)
+  end
+
+  it 'skips blank lines' do
+    data = expected_data << ["\n", 0]
+    stub_talks_data(data)
+    talk_data = TwScheduleIt::ImportFromFile.build('/tmp/talk_data.txt')
+
+    expect(talk_data.last.first).not_to be_empty
   end
 end
