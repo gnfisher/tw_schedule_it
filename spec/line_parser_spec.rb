a73cb2de21afbed14a458fd4aa3106c012d1bd04
Greg Fisher <me@gnfisher.com>
2018-01-08 09:57:04 -0300

    make test more readable for lineparser

--- spec/line_parser_spec.rb
+++ spec/line_parser_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe TwScheduleIt::LineParser do
     it "returns an array with two elements, title and duration" do
       text = "First Talk Name 60min"
 
-      result = TwScheduleIt::LineParser.new(text).call
+      talk_title, talk_duration = TwScheduleIt::LineParser.new(text).call
 
-      expect(result[0]).to eq text
-      expect(result[1]).to eq 60
+      expect(talk_title).to eq text
+      expect(talk_duration).to eq 60
     end
 
     it "converts lightning talks to 5 minutes length" do
       text = "Talk Name lightning"
 
-      result = TwScheduleIt::LineParser.new(text).call
+      talk_title, talk_duration = TwScheduleIt::LineParser.new(text).call
 
-      expect(result[1]).to eq 5
+      expect(talk_duration).to eq 5
     end
   end
 end
